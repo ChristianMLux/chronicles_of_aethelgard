28353a39cb016cdb8f6a3d3870a049d4a207a472
MOBILECL\chris <christian.m.lux@gmail.com>
2025-08-19 20:53:55 +0200

    fix: another promise await fixed

--- src/app/city/[id]/page.tsx
+++ src/app/city/[id]/page.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "next/navigation";
 
-export default function CityPage({ params }: { params: { id: string } }) {
-  redirect(`/city/${params.id}/buildings`);
+export default async function CityPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  redirect(`/city/${id}/buildings`);
 }
